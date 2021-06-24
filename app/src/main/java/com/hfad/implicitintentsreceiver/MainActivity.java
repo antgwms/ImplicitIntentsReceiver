fe9bbfc4b0312da0ce4afa75769607dd7b6f1914
antgwms <antgwms@outlook.com>
2021-06-24 22:41:13 +0100

    Update AndroidManifest.xml
    Add the implicit intent filter in the manifest file and include an intent in MainActivity to process the incoming intent for data or extras.

--- app/src/main/java/com/hfad/implicitintentsreceiver/MainActivity.java
+++ app/src/main/java/com/hfad/implicitintentsreceiver/MainActivity.java
@@ -2,7 +2,10 @@ package com.hfad.implicitintentsreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //process the incoming intent for any data or extras it includes
+        Intent intent = getIntent();
+        Uri uri;
+        uri = intent.getData();
+        if (uri != null) {
+            String uri_string = "URI: " + uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uri_string);
+        }
     }
 }
